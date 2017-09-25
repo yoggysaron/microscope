ae6f4f376c1649ceaa39249deab8d3f8e98a8e12
yoggysaron <durand-coutelle@netcourrier.com>
2017-09-25 10:51:31 +0200

    MAJ 06
    avant reset chat

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,18 @@
+Template.postSubmit.onCreated(function(){
+    Session.set('postSubmitErrors', {});
+});
+Template.postSubmit.helpers({
+    errorMessage: function(field){
+        return Session.get('postSubmitErrors')[field];
+    },
+    errorClass: function (field) {
+        return !!Session.get('postSubmitErrors') [field] ? 'has-error' : '';
+    }
+});
+
+
+
+
 Template.postSubmit.events({
     'submit form': function(e) {
         e.preventDefault();
@@ -7,12 +22,16 @@ Template.postSubmit.events({
             title: $(e.target).find('[name=title]').val()
         };
 
+        var errors = validatePost(post);
+        if (errors.title || errors.url)
+            return Session.set('postSubmitErrors', errors);
+
         Meteor.call('postInsert', post, function(error, result) {
             if (error)
-                return alert(error.reason);
+                return throwError(error.reason);
 
             if (result.postExists)
-                alert('Ce lien a déjà été utilisé');
+                throwError('Ce lien a déjà été utilisé');
 
             Router.go('postPage', {_id: result._id});
         });
